878713a57f4babeb8fba2b153917f98ca38efee7
Carlgo11 <carlgo11@users.noreply.github.com>
2024-08-17 03:14:10 +0200

    Configure dovecot

--- inbox/src/config/mxTLS.js
+++ inbox/src/config/mxTLS.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const tlsConfig = {
   key: fs.readFileSync(process.env.INBOX_TLS_KEY_PATH || process.env.TLS_KEY_PATH),
   cert: fs.readFileSync(process.env.INBOX_TLS_CERT_PATH || process.env.TLS_CERT_PATH),
-  minVersion: process.env.TLS_MIN_VERSION,
-  maxVersion: process.env.TLS_MAX_VERSION,
-  ciphers: process.env.TLS_CIPHERS,
+  minVersion: process.env.INBOX_TLS_MIN_VERSION || process.env.TLS_MIN_VERSION,
+  maxVersion: process.env.INBOX_TLS_MAX_VERSION || process.env.TLS_MAX_VERSION,
+  ciphers: process.env.INBOX_TLS_CIPHERS || process.env.TLS_CIPHERS,
   handshakeTimeout: 5000,
   onSecure: (socket, session, callback) => {
     console.debug(`${session.remoteAddress} connection upgraded to TLS`);
--- outbox/src/config/tls.js
+++ outbox/src/config/tls.js
@@ -7,9 +7,9 @@ const tlsConfig = {
   cert: fs.readFileSync(process.env.OUTBOX_TLS_CERT_PATH || process.env.TLS_CERT_PATH),
   ca: fs.readFileSync(process.env.OUTBOX_TLS_CA_PATH),
   requestCert: true,
-  minVersion: process.env.TLS_MIN_VERSION,
-  maxVersion: process.env.TLS_MAX_VERSION,
-  ciphers: process.env.TLS_CIPHERS,
+  minVersion: process.env.OUTBOX_TLS_MIN_VERSION || process.env.TLS_MIN_VERSION,
+  maxVersion: process.env.OUTBOX_TLS_MAX_VERSION || process.env.TLS_MAX_VERSION,
+  ciphers: process.env.OUTBOX_TLS_MAX_VERSION || process.env.TLS_CIPHERS,
   handshakeTimeout: 5000,
   onSecure: (socket, session, callback) => {
     console.debug(JSON.stringify(session));
