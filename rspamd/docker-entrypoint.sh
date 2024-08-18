06f5e90d192f604a9f04a674c5989db412b97fc4
Carlgo11 <carlgo11@users.noreply.github.com>
2024-08-19 01:02:27 +0200

    Custom Rspamd docker image

---
+++ rspamd/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+password_hash=$(/usr/bin/rspamadm pw -e -p "$RSPAMD_PASSWORD")
+
+cat <<EOF | tee /etc/rspamd/local.d/worker-controller.inc
+bind_socket = "0.0.0.0:11334"
+milter = no;
+enable_password = "$password_hash";
+password = "$password_hash";
+EOF
+
+echo 'severs = "172.18.0.3";' > /etc/rspamd/local.d/dmarc.conf
+
+exec "$@"
