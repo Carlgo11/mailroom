b8faacff246af86b113fdf6d465f25742ebab09f
Carlgo11 <carlgo11@users.noreply.github.com>
2024-08-31 02:04:06 +0200

    Only validate inbox auth if requested

--- inbox/src/services/authService.js
+++ inbox/src/services/authService.js
@@ -5,33 +5,32 @@ class AuthService {
     const error = new Error();
     error.responseCode = 550;
 
-    const { dkim, spf, arc, headers,dmarc } = await mailAuth.Validate(message, session);
+    const {dkim, spf, arc, dmarc} = await mailAuth.Validate(message, session);
 
-    if(process.env.NODE_ENV !== 'production')
-      console.debug(headers);
-
-    if (spf.status.result !== 'pass') {
+    if (process.env.INBOX_AUTH.includes('spf') && spf.status.result !== 'pass') {
       error.message = '5.7.1 SPF check failed.';
       throw error;
     }
 
-    if (arc.status.result !== 'pass') {
+    if (process.env.INBOX_AUTH.includes('arc') && arc.status.result !== 'pass') {
       error.message = '5.7.1 ARC check failed.';
-      console.log(error)
+      console.log(error);
       throw error;
     }
 
-    for (const {status} of dkim.results) {
-      if (status.result !== 'pass') {
-        error.message = '5.7.1 DKIM check failed.';
-        console.log(error)
-        throw error;
+    if (process.env.INBOX_AUTH.includes('dkim')) {
+      for (const {status} of dkim.results) {
+        if (status.result !== 'pass') {
+          error.message = '5.7.1 DKIM check failed.';
+          console.log(error);
+          throw error;
+        }
       }
     }
 
-    if(dmarc.status.result !== 'pass'){
+    if (process.env.INBOX_AUTH.includes('dmarc') && dmarc.status.result !== 'pass') {
       error.message = '5.7.1 DMARC check failed.';
-      console.log(error)
+      console.log(error);
       throw error;
     }
 
