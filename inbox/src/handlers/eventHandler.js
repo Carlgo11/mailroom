7991e9ede0f5755b81469cc9c5fd3fbef13f1d9d
Carlgo11 <carlgo11@users.noreply.github.com>
2024-10-02 00:14:22 +0200

    Clean up code

---
+++ inbox/src/handlers/eventHandler.js
@@ -0,0 +1,147 @@
+import {handleIncomingEmail} from '../controllers/emailController.js';
+import {userExists} from '../services/userService.js';
+import Log from '../services/logService.js';
+import Spamhaus from '../validators/spamhaus.js';
+import ipScore from '../validators/ipScore.js';
+import ipQS from '../validators/ipQS.js';
+
+/**
+ * Handles the RCPT TO command during SMTP transaction.
+ *
+ * @param {Object} address - The address object containing recipient's email.
+ * @param {Object} session - The session object for the SMTP transaction.
+ * @param {Function} callback - The callback to signal success or failure.
+ * @returns Function - Returns callback
+ */
+export async function handleRcptTo(address, session, callback) {
+  try {
+    const recipientExists = await userExists(address.address);
+    if (!recipientExists) {
+      Log.info(`Unknown recipient <${address.address}>`, session.id);
+      const err = new Error('5.1.1 Mailbox not found');
+      err.responseCode = 550;
+      return callback(err);
+    }
+    return callback();
+  } catch (err) {
+    Log.error(`Error in handleRcptTo: ${err.message}`, session.id);
+    return callback(err);
+  }
+}
+
+/**
+ * Handles the MAIL FROM command during SMTP transaction.
+ *
+ * @param {Object} address - The address object containing sender's email.
+ * @param {Object} session - The session object for the SMTP transaction.
+ * @param {Function} callback - The callback to signal success or failure.
+ * @returns Function - Returns callback
+ */
+export function handleMailFrom(address, session, callback) {
+  if (session.secure) {
+    return callback();
+  }
+  const err = new Error('5.7.0 Encryption required');
+  err.responseCode = 530;
+  Log.info('Encryption required for MAIL FROM', session.id);
+  return callback(err);
+}
+
+/**
+ * Handles the DATA command during SMTP transaction.
+ *
+ * @param {Object} stream - The stream object containing the email content.
+ * @param {Object} session - The session object for the SMTP transaction.
+ * @param {Function} callback - The callback to signal success or failure.
+ * @returns Function - Returns callback
+ */
+export async function handleData(stream, session, callback) {
+  try {
+    Log.info('Client sending message', session.id);
+    await handleIncomingEmail(stream, session);
+    Log.info('Message accepted', session.id);
+    return callback(null, '2.6.0 Message accepted');
+  } catch (err) {
+    Log.error(`Message rejected: ${err.message}`, session.id);
+    if ('responseCode' in err) {
+      return callback(err);
+    } else {
+      const error = new Error('5.0.0 Internal server error');
+      error.responseCode = 554;
+      return callback(error);
+    }
+  }
+}
+
+/**
+ * Handles the initial connection to the SMTP server.
+ *
+ * @param {Object} session - The session object containing connection details.
+ * @param {Function} callback - The callback to signal connection acceptance or rejection.
+ * @returns Function - Returns callback
+ */
+export function handleConnect({remoteAddress, id, clientHostname}, callback) {
+  Log.info(`${remoteAddress} connected. <${clientHostname}>`, id);
+  validateConnection(remoteAddress, id).
+      then(() => callback()) // No error, proceed
+      .catch((err) => callback(err)); // Pass error to callback
+}
+
+/**
+ * Validates the IP address of the connecting client against blacklists and fraud score services.
+ *
+ * @param {string} remoteAddress - The IP address of the connecting client.
+ * @param {string} id - The session ID for logging.
+ * @returns {Promise<boolean>} Resolves to true if validation passes, otherwise throws an error.
+ * @throws {Error} Throws an error if the IP address is blacklisted or has a high fraud score.
+ */
+async function validateConnection(remoteAddress, id) {
+  const ipqsScoreLimit = parseInt(process.env.IPQS_SCORE_LIMIT, 10) || 50;
+
+  const [spamhausListed, ipqsScore, ipscore] = await Promise.all([
+    Spamhaus.lookupIP(remoteAddress),
+    ipQS.lookupIP(remoteAddress),
+    ipScore.lookupIP(remoteAddress),
+  ]);
+
+  if (spamhausListed) {
+    Log.info(`${remoteAddress} blacklisted by Spamhaus`, id);
+    const err = new Error('5.7.1 IP blacklisted by Spamhaus');
+    err.responseCode = 554;
+    throw err;
+  }
+
+  if (ipqsScore > ipqsScoreLimit) {
+    Log.info(`${remoteAddress} has high IPQS score: ${ipqsScore}`, id);
+    const err = new Error('5.7.1 IP reported as malicious by IPQS');
+    err.responseCode = 554;
+    throw err;
+  }
+
+  return true;
+}
+
+/**
+ * Handles the close of an SMTP session.
+ *
+ * @param {Object} session - The session object for the SMTP transaction.
+ * @returns {void}
+ */
+export function handleClose(session) {
+  Log.info(`${session.remoteAddress} disconnected.`, session.id);
+}
+
+/**
+ * Handles secure connection upgrade (STARTTLS) during SMTP transaction.
+ *
+ * @param {Object} socket - The socket object of the connection.
+ * @param {Object} session - The session object for the SMTP transaction.
+ * @param {Function} callback - The callback to signal the completion of the upgrade.
+ * @returns Function - Returns callback
+ */
+export function handleSecure(socket, session, callback) {
+  const protocol = socket.getProtocol();
+  const cipherName = socket.getCipher().name;
+  Log.info(`Connection upgraded to ${protocol} (${cipherName})`, session.id);
+  return callback();
+}
--- inbox/src/servers/server.js
+++ inbox/src/servers/server.js
@@ -1,70 +1,106 @@
+import fs from 'fs';
+import {createRequire} from 'module';
 import Log from '../services/logService.js';
-import * as smtpRouter from '../routes/smtpRouter.js';
 import {tlsConfig} from '../config/tls.js';
-import Module from 'node:module';
+import {
+  handleClose,
+  handleConnect,
+  handleData,
+  handleMailFrom,
+  handleRcptTo,
+  handleSecure,
+} from '../handlers/eventHandler.js';
 
-const require = Module.createRequire(import.meta.url);
+// Use createRequire to import CommonJS modules in ESM
+const require = createRequire(import.meta.url);
+const {SMTPServer} = require('smtp-server');
+
+const pidFile = '/var/tmp/inbox.pid';
 let server;
 
 export function startServer() {
-  const SMTPServer = require('smtp-server').SMTPServer;
-  server = new SMTPServer({
-    ...tlsConfig,
-    onRcptTo: smtpRouter.handleRcptTo,
-    onData: smtpRouter.handleData,
-    onMailFrom: smtpRouter.handleMailFrom,
-    Logger: process.env.NODE_ENV === 'development',
-    disabledCommands: ['AUTH', 'HELP'],
-    onConnect(session, callback) {
-      Log.info(
-          `${session.remoteAddress} connected. <${session.clientHostname}>`,
-          session.id);
-      smtpRouter.handleConnect(session).then((r) => callback(r));
-    },
-    onClose(session) {
-      Log.info(`${session.remoteAddress} disconnected.`, session.id);
-    },
-    onSecure: (socket, session, callback) => {
-      Log.info(
-          `connection upgraded to ${socket.getProtocol()} (${socket.getCipher().name})`,
-          session.id);
-      return callback();
-    },
-  });
+  // Write the PID file
+  fs.writeFileSync(pidFile, process.pid.toString(), {encoding: 'utf8'});
 
-  server.on('error', (err) => {
-    if (err['library'] === 'SSL routines') {
-      switch (err.code) {
-        case 'ERR_SSL_NO_SHARED_CIPHER':
-          Log.info(
-              `${err.remote} does not support any compatible TLS ciphers.`);
-          break;
-        case 'ERR_SSL_UNSUPPORTED_PROTOCOL':
-          Log.info(
-              `${err.remote} does not support any compatible TLS versions.`);
-          break;
-        default:
-          Log.info(`TLS Error: ${err.reason} (${err.remote})`);
-          break;
-      }
-    } else {
-      Log.error(`Error: ${err.reason || err}`);
-    }
-  });
+  try {
+    // Create SMTP server instance
+    server = new SMTPServer({
+      ...tlsConfig,
+      onConnect: (session, callback) => handleConnect(session, callback),
+      onMailFrom: (address, session, callback) => handleMailFrom(address,
+          session, callback),
+      onRcptTo: (address, session, callback) => {
+        handleRcptTo(address, session, callback).catch(callback);
+      },
+      onData: (stream, session, callback) => {
+        handleData(stream, session, callback).catch(callback);
+      },
+      onClose: handleClose,
+      onSecure: (socket, session, callback) => handleSecure(socket, session,
+          callback),
+      disabledCommands: ['AUTH', 'HELP'],
+      logger: process.env.NODE_ENV === 'development',
+    });
+  } catch (e) {
+    console.error(e);
+    cleanup();
+    process.exit(1);
+  }
 
+  // Attach error handler
+  server.on('error', handleError);
+
+  // Start listening
   server.listen(process.env.INBOX_PORT, () => {
     Log.info(`Inbox server listening on port ${process.env.INBOX_PORT}`);
   });
 }
 
+function handleError(err) {
+  if (err.library === 'SSL routines') {
+    switch (err.code) {
+      case 'ERR_SSL_NO_SHARED_CIPHER':
+        Log.info(
+            `${err.remote} does not support any compatible TLS ciphers.`,
+        );
+        break;
+      case 'ERR_SSL_UNSUPPORTED_PROTOCOL':
+        Log.info(
+            `${err.remote} does not support any compatible TLS versions.`,
+        );
+        break;
+      default:
+        Log.info(`TLS Error: ${err.reason} (${err.remote})`);
+        break;
+    }
+  } else {
+    Log.error(`Error: ${err.reason || err.message || err}`);
+  }
+}
+
+function cleanup() {
+  if (fs.existsSync(pidFile)) {
+    fs.unlinkSync(pidFile);
+    console.log(`PID file ${pidFile} removed.`);
+  }
+}
+
 function stopServer() {
   server.close(() => {
     console.log('Inbox server stopped');
+    cleanup();
     process.exit(0);
   });
 }
 
-process.on('SIGTERM', () => stopServer());
-process.on('SIGINT', () => stopServer());
+// Handle process signals and exceptions
+process.on('SIGTERM', stopServer);
+process.on('SIGINT', stopServer);
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  cleanup();
+  process.exit(1);
+});
 
+// Export the startServer function as default
 export default startServer;
