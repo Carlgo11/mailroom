b328b7ca89f5c93b5ae72bb4defe5dcc3a962a13
Carlgo11 <carlgo11@users.noreply.github.com>
2024-12-12 14:46:25 +0100

    Finish auth function

--- outbox/src/handlers/auth.js
+++ outbox/src/handlers/auth.js
@@ -3,8 +3,13 @@ import redis from '../services/redis.js';
 import bcrypt from 'bcrypt';
 
 export async function handleAuth(args, session) {
+  if (!session.tls)
+    return session.send(new Response('STARTTLS required', 523, [5, 7, 10]));
+
+  if (session.state !== session.states.STARTTLS)
+    return session.send(new Response(null, 503, [5, 5, 1]));
+
   const method = args[0].toUpperCase();
-  console.log(method);
   switch (method) {
     case 'PLAIN':
       return plain(args, session);
@@ -14,31 +19,46 @@ export async function handleAuth(args, session) {
   }
 }
 
+/**
+ * handle PLAIN authentication.
+ *
+ * @param args
+ * @param session
+ * @returns {Promise<*>}
+ */
 async function plain(args, session) {
   if (args.length > 2)
     return session.send(
       new Response('Invalid parameter length', 500, [5, 5, 6]));
 
   try {
-    const [authz, username, password] = atob(args[1]).split('\0');
-    console.log(username, password);
-
+    const [authzId, username, password] = atob(args[1]).split('\0');
     const auth = await authenticate(username, password);
-    if (!auth) throw null;
+    // Failed auth
+    if (!auth) throw false;
+
+    // Successful auth
     session.username = username;
+    return session.send(
+      new Response('Authentication successful', 235, [2, 7, 0]));
 
   } catch (e) {
     return session.send(new Response('Authentication failed', 535, [5, 7, 8]));
   }
 }
 
+/**
+ * Authenticate client with provided credentials.
+ *
+ * @param {string} username
+ * @param {string} password
+ */
 async function authenticate(username, password) {
   try {
     const data = await redis.get(`user:${username}`);
     const json = JSON.parse(data);
-    return bcrypt.compare(password, json.password);
-
-  }catch (e) {
-    throw new Response('Authentication failed', 535);
+    return bcrypt.compare(password, json.password.replace('{BLF-CRYPT}', ''));
+  } catch (_) {
+    throw false;
   }
 }
--- outbox/src/server.js
+++ outbox/src/server.js
@@ -1,21 +1,49 @@
-import {Server, Response, Logger, registerCommand} from '@carlgo11/smtp-server';
+import { Server, Logger, registerCommand } from '@carlgo11/smtp-server';
 import fs from 'fs';
 import { handleAuth } from './handlers/auth.js';
+import handleMail from './handlers/mail.js';
 
 const server = Server({
   tlsOptions: {
-    // key: fs.readFileSync(process.env.OUTBOX_TLS_KEY_PATH),
-    // cert: fs.readFileSync(process.env.OUTBOX_CERT_PATH),
+    key: fs.readFileSync(process.env.OUTBOX_TLS_KEY_PATH),
+    cert: fs.readFileSync(process.env.OUTBOX_TLS_CERT_PATH),
     handshakeTimeout: 5000,
     minVersion: 'TLSv1.3',
     maxVersion: 'TLSv1.3',
     ALPNProtocols: ['h2'],
   },
-
+  extensions: ['ENHANCEDSTATUSCODES', 'PIPELINING', 'AUTH PLAIN', '8BITMIME'],
+  onMAILFROM: handleMail,
 
 });
-registerCommand('AUTH', handleAuth)
+
+function cleanup() {
+  /*if (fs.existsSync(pidFile)) {
+    fs.unlinkSync(pidFile);
+    console.log(`PID file ${pidFile} removed.`);
+  }*/
+}
+
+function stopServer() {
+  server.close(() => {
+    console.log('Inbox server stopped');
+    cleanup();
+    process.exit(0);
+  });
+}
+
+registerCommand('AUTH', handleAuth);
+
+// Handle process signals and exceptions
+process.on('SIGTERM', stopServer);
+process.on('SIGINT', stopServer);
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  cleanup();
+  process.exit(1);
+});
+
 server.listen(5877, () => {
-  const {address, family, port} = server.address();
+  const { address, family, port } = server.address();
   Logger.info(`SMTP Server listening on ${address}:${port} via ${family}`);
 }).on('error', (err) => console.log(`${err}`));
