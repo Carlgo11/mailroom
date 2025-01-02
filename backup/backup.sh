d97e6e77597cc3c86f90ba1b70f271912ecf628b
Carlgo11 <carlgo11@users.noreply.github.com>
2025-01-02 03:57:15 +0100

    Add option to GPG-encrypt backups

--- backup/backup.sh
+++ backup/backup.sh
@@ -6,15 +6,33 @@ VHOST_DIR="${VHOST_DIR:-/var/mail/vhosts/}"
 BACKUP_TMP="${BACKUP_TMP:-/tmp/backups}"
 RCLONE_REMOTE="${RCLONE_REMOTE:-backups:/backups}"
 RCLONE_CONFIG="${RCLONE_CONFIG:-/rclone.conf}"
+export GNUPGHOME="/tmp/.gnupg"
 
 DATE=$(date +%FT%TZ -u)
 BACKUP_FILE="${DATE}.tar.gz"
 
 mkdir -p "$BACKUP_TMP"
-cp "$RCLONE_CONFIG" /tmp/rclone.conf
+
+if ! cp "$RCLONE_CONFIG" /tmp/rclone.conf; then
+  echo "rclone config not set"
+  exit 1
+fi
 
 echo "Creating $BACKUP_FILE"
 tar -czf "${BACKUP_TMP}/${BACKUP_FILE}" -C "$VHOST_DIR" .
+
+if [ -f /public.key ]; then
+
+    echo "/public.key found. Encrypting the backup."
+
+    if gpg -e -f /public.key "${BACKUP_TMP}/${BACKUP_FILE}"; then
+      rm "${BACKUP_TMP}/${BACKUP_FILE}" || exit 1
+    else
+      echo "GPG encryption failed"
+    fi
+else
+    echo "No public key found. Proceeding without encryption."
+fi
 
 echo "Uploading backup to $RCLONE_REMOTE"
 rclone copy "${BACKUP_TMP}" "$RCLONE_REMOTE" --config "/tmp/rclone.conf" || exit 1
