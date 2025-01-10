86ee4586b174ffab2bf7c7053c4ad235c236faa4
Carlgo11 <carlgo11@users.noreply.github.com>
2025-01-11 00:05:00 +0100

    Create spool directories on startup

--- outbox/entrypoint.sh
+++ outbox/entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-SPOOL=" /var/spool/smtpd"
+SPOOL="/var/spool/smtpd"
 
-chmod 711  "$SPOOL"
+chmod 711 "$SPOOL"
 
-chmod 770  "$SPOOL/offline"
-chown 0:102  "$SPOOL/offline"
+mkdir "$SPOOL/offline" -p
+chmod 770 "$SPOOL/offline"
+chown 0:102 "$SPOOL/offline"
 
-chmod 700  "$SPOOL/purge"
-chown 101:0  "$SPOOL/purge"
+mkdir "$SPOOL/purge"
+chmod 700 "$SPOOL/purge"
+chown 101:0 "$SPOOL/purge"
 
-chmod 700  "$SPOOL/queue"
-chown 101:0  "$SPOOL/queue"
+mkdir "$SPOOL/queue"
+chmod 700 "$SPOOL/queue"
+chown 101:0 "$SPOOL/queue"
 
 smtpd -d -v -P mda &
 
